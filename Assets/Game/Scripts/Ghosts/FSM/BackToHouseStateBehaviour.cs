17dee1167b1faba1c02138c63c47c7a10ede668e
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2020-02-09 10:03:14 -0500

    Added States in FSMs and added their respective state behaviours

---
+++ Assets/Game/Scripts/Ghosts/FSM/BackToHouseStateBehaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToHouseStateBehaviour : GhostBehaviour
+{
+
+    //override public void OnStateEnter(Animator pFSM, AnimatorStateInfo pStateInfo, int pLayerIndex)
+    //{
+    //    
+    //}
+
+    //override public void OnStateUpdate(Animator pFSM, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    //override public void OnStateExit(Animator pFSM, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+}
+
