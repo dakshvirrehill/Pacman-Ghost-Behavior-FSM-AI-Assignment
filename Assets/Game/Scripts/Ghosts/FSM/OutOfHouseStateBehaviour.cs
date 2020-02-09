436fe02768907ced7b9e3372efa158e2c1c42159
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2020-02-09 10:50:30 -0500

    Added code to get to scatter mode after a set period of time

--- Assets/Game/Scripts/Ghosts/FSM/OutOfHouseStateBehaviour.cs
+++ Assets/Game/Scripts/Ghosts/FSM/OutOfHouseStateBehaviour.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class OutOfHouseStateBehaviour : GhostBehaviour
 {
     public float mWaitTime;
+    float mCurTimer = 0;
     override public void OnStateEnter(Animator pFSM, AnimatorStateInfo pStateInfo, int pLayerIndex)
     {
         SetupComponentReferences(pFSM);
+        mCurTimer = 0.0f;
     }
 
-    //override public void OnStateUpdate(Animator pFSM, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator pFSM, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        mCurTimer -= Time.deltaTime;
+        if(mCurTimer <= 0.0f)
+        {
+            pFSM.SetTrigger("Scatter");
+            mCurTimer = 0.0f;
+        }
+    }
 
-    //override public void OnStateExit(Animator pFSM, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateExit(Animator pFSM, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
 
 }
 
