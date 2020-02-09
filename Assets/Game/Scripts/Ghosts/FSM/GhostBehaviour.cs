452ee10010bfdf231c184c77fe31bc5660b46b00
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2020-02-08 22:20:54 -0500

    Added code for setting up basic values and also added start state to blinky fsm

--- Assets/Game/Scripts/Ghosts/FSM/GhostBehaviour.cs
+++ Assets/Game/Scripts/Ghosts/FSM/GhostBehaviour.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class GhostBehaviour : StateMachineBehaviour
 {
+    protected Transform mGhostTransform;
+    protected GhostController mController;
+    protected Animator mAnimator;
+    protected Animator mFSM;
+
+    protected void SetupComponentReferences(Animator pFSM)
+    {
+        if(mController != null)
+        {
+            return;
+        }
+        mGhostTransform = pFSM.transform.parent;
+        mFSM = pFSM;
+        mController = mGhostTransform.GetComponent<GhostController>();
+        mAnimator = mGhostTransform.GetComponent<Animator>();
+    }
 
 }
