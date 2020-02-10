db8a84d48f511476b2939205c9594ea39bf62aee
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2020-02-10 17:56:35 -0500

    Completed Scatter State Behaviour

---
+++ Assets/Game/Scripts/Ghosts/FSM/ScatterStateBehaviour.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScatterStateBehaviour : GhostBehaviour
+{
+    public Vector2[] mScatterPositions;
+
+    int mScatterIx = 0;
+
+
+    override public void OnStateEnter(Animator pFSM, AnimatorStateInfo pStateInfo, int pLayerIndex)
+    {
+        SetupComponentReferences(pFSM);
+        mController.pathCompletedEvent.AddListener(ScatterPathCompleted);
+        if(mReverseDirection)
+        {
+            ReverseDirection();
+        }
+        mScatterIx = 0;
+    }
+
+    override public void OnStateUpdate(Animator pFSM, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(mController.mIsChasing)
+        {
+            mReverseDirection = true;
+            pFSM.SetTrigger(mController.mChase);
+        }
+    }
+
+    override public void OnStateExit(Animator pFSM, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        mController.pathCompletedEvent.RemoveListener(ScatterPathCompleted);
+        StateExit();
+    }
+
+    void ScatterPathCompleted()
+    {
+        if(mScatterIx >= mScatterPositions.Length)
+        {
+            mScatterIx = 0;
+        }
+        mController.moveToLocation = mScatterPositions[mScatterIx];
+        mController.moveComplete();
+        mScatterIx++;
+    }
+
+
+}
+
